1cc4d7fd7942de438c2fc164435e06064d16a6a8
gxgarciat <info@gxgarciat.dev>
2021-06-28 19:44:21 +0100

    Radio buttons

---
+++ 9_radiobuttons.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+
+# This code will use radio buttons
+
+root = Tk()
+root.title("Radio Buttons")
+
+r = IntVar() # It will collect what has been stored in this variable
+# In case it is a string, it should be StrVar()
+r.set("2")
+
+def clicked(value):
+    myLabel = Label(root,text=value)
+    myLabel.pack()
+
+Radiobutton(root,text="Option 1",variable=r,value=1,command=lambda:clicked(r.get())).pack()
+Radiobutton(root,text="Option 2",variable=r,value=2,command=lambda:clicked(r.get())).pack()
+#Radiobutton.pack()
+
+myLabel = Label(root,text=r.get())
+myLabel.pack()
+
+myButton = Button(root,text="Click here",command=lambda:clicked(r.get()))
+myButton.pack()
+
+root.mainloop()
